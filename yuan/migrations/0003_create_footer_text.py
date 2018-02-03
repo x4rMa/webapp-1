48d2594bc1eac3cae2a777073411135335f8ed33
Atila Satilmis <atila.satilmis@gmail.com>
2018-02-03 17:46:59 +0300

    blah

---
+++ yuan/migrations/0003_create_footer_text.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+
+
+def create_content(apps, schema_editor):
+    Content = apps.get_model('yuan.content')
+
+    content = Content.objects.create(
+        content_type='footer text',
+        content_body='Text',
+    )
+
+
+def remove_content(apps, schema_editor):
+    Content = apps.get_model('yuan.content')
+
+    Content.objects.filter(content_type='footer_text').delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('yuan', '0002_create_initial_superuser'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_content, remove_content),
+    ]
