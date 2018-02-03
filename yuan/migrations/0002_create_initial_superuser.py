0a1879a60f974796eeee69c84688576b24cf334a
Atila Satilmis <atila.satilmis@gmail.com>
2018-02-03 17:17:52 +0300

    blah

---
+++ yuan/migrations/0002_create_initial_superuser.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model('auth.user')
+
+    user = User.objects.create(
+        username='admin',
+        password='pbkdf2_sha256$36000$k5EI3Ee3tQbh$+n17tTaZMuyzd0qKVCiFPC4ntbk2fRgpE6z3F8AwEXk=',
+        is_superuser=True,
+        is_staff= True,
+        is_active= True,
+    )
+
+
+def remove_superuser(apps, schema_editor):
+    User = apps.get_model('auth.user')
+
+    User.objects.filter(username='admin').delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('yuan', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser, remove_superuser),
+    ]
